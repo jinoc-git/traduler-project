631a352af1d6f66144fec788cdf66611cda1a05c
jinoc <choiae369@gmail.com>
2023-08-23 11:49:07 +0900

    Design: 메인페이지 배경 설정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 
 import { getPlans } from '@api/plans';
-import Pay from '@components/pay/pay';
+// import Pay from '@components/pay/pay';
+import Profile from '@components/main/profile/Profile';
 import { useQuery } from '@tanstack/react-query';
 
 const Main = () => {
@@ -11,7 +12,9 @@ const Main = () => {
   console.log(data);
 
   return (
-    <div>
+    <main>
+      <div className='absolute top-0 w-screen h-[363px] bg-[#393939] z-[-1]'></div>
+      <Profile />
       <button
         className="p-5 bg-slate-500"
         onClick={() => {
@@ -20,8 +23,8 @@ const Main = () => {
       >
         계획 추가하기
       </button>
-      <Pay />
-    </div>
+      {/* <Pay /> */}
+    </main>
   );
 };
 

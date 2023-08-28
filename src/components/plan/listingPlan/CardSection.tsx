179df3d6ef89ccc76af3b5333e965f5142da8b98
Nick YOOO <outgrowthr@gmail.com>
2023-08-28 19:47:31 +0900

    Chore: CardSection 컴퍼넌트 > Card 컴퍼넌트 상속, 및 목데이터(userId) 삽입

--- src/components/plan/listingPlan/CardSection.tsx
+++ src/components/plan/listingPlan/CardSection.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 import { getPlans } from '@api/plans';
 import { useQuery } from '@tanstack/react-query';
-import { type PlanType } from 'types/supabase';
 
 import Card from './Card';
 
 const CardSection = () => {
-  const { data, isLoading, isError } = useQuery<PlanType[] | null>(
+  const { data, isLoading, isError } = useQuery(
     ['plans'],
-    getPlans,
+    // async () => await getPlans(userId),
+    // 유저아이디 가져오기 목데이터
+    async () => await getPlans('10d4b5c3-12d6-486b-862b-6f63c0c9f4fc'),
   );
-
   // console.log('MainCardsData', data);
 
   if (isLoading) {
@@ -20,7 +20,10 @@ const CardSection = () => {
   if (isError) {
     return <div>로딩중 ...</div>;
   }
-
+  if (data == null) {
+    return <div>로딩중 ...</div>;
+  }
+  console.log('나 데이터', data);
   return (
     <div>
       <div></div>

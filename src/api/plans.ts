5a6b0b5ad12d52aa2301f7776bb6a8e2863ff97f
Nick YOOO <outgrowthr@gmail.com>
2023-08-31 21:28:33 +0900

    Chore: addBookMark api 분리, 안쓰는 주석, consol.log 삭제

--- src/api/plans.ts
+++ src/api/plans.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { uuid } from '@supabase/gotrue-js/dist/module/lib/helpers';
 import {
   type PlanMatesType,
@@ -30,10 +29,8 @@ export const addPlan = async (
     plan_state: 'planning',
   };
 
-  // plan data 추가
   const { data, error } = await supabase.from('plans').insert(plan);
 
-  // 날짜별 pins data 추가
   for (let i = 0; i < dates.length; i++) {
     const { error: errorPins } = await supabase.from('pins').insert({
       plan_id: planId,
@@ -45,7 +42,6 @@ export const addPlan = async (
     }
   }
 
-  // plan_mates data 추가
   const newplanMates: PlanMatesType = {
     id: planId,
     users_id: invitedUser.map((user) => user.id),
@@ -83,7 +79,6 @@ export const getPlans = async (userId: string | undefined) => {
   const { data: plans, error } = await supabase.from('plans').select().match({
     id: userId,
   });
-
   if (error !== null) {
     console.log(error);
     throw new Error('오류발생');
@@ -123,13 +118,10 @@ export const updateDatePlan = async (planId: string, dates: string[]) => {
   }
 };
 
-// 여기부터
 export const getPlansByUserIds = async (userIds: string[]) => {
-  // 중복된 아이디값이들어와서 분류
   const { data, error } = await supabase
     .from('plans')
     .select()
-    // .in는 특정 열의 값이 지정된 배열의 값과 일치하는 행을 필터링
     .in('users_id', userIds);
 
   if (error != null) {
@@ -144,48 +136,21 @@ export const getPlansWithMates = async (userId: string) => {
   const { data: matesData, error: matesError } = await supabase
     .from('plan_mates')
     .select('users_id')
-    // 배열의 비교는 contains 연산자를 사용
     .contains('users_id', [userId]);
 
   if (matesError != null) {
     console.log('에러 발생', matesError);
     return null;
   }
-  // flatMap을 사용하면 중복 구조로 되어있는 리스트를 하나의 스트림처럼 다룰 수 있다.
   const userIds = matesData?.flatMap((mate) => mate.users_id);
 
-  if (!userIds?.length) {
+  if (userIds.length === 0) {
     return null;
   }
 
   const plansData = await getPlansByUserIds(userIds);
 
   return plansData;
-};
-
-// 여기까지
-export const addBookMark = async (
-  newBookMarkId: string,
-  planId: string,
-  userId: string,
-) => {
-  const { error } = await supabase.from('book_mark').insert({
-    id: newBookMarkId,
-    plan_id: planId,
-    user_id: userId,
-  });
-  if (error !== null) {
-    console.log(error);
-    throw new Error('오류발생');
-  }
-};
-
-export const deleteBookMark = async (id: string, planId: string) => {
-  const { error } = await supabase.from('book_mark').delete().eq('id', planId);
-  if (error !== null) {
-    console.log(error);
-    throw new Error('오류발생');
-  }
 };
 
 export const updatePlan = async (

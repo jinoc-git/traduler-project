b57c6ff89b9131026e6d1d515d8bb31995372087
jinoc <choiae369@gmail.com>
2023-09-01 21:54:47 +0900

    Feat: 헤더의 로그아웃 기능 삭제

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,23 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import { signOutForSB } from '@api/supabaseAuth';
 import { ic_menu_1x } from '@assets/icons/1x';
 import { ic_profile_3x } from '@assets/icons/3x';
-import { signOutDropDown } from '@assets/index';
-import useBooleanState from '@hooks/useBooleanState';
 import { sideBarStore } from '@store/sideBarStore';
 import { userStore } from '@store/userStore';
 
 const Header = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-
-  const {
-    value: isMenuOpen,
-    toggleValue: toggleIsMenuOpen,
-    setNeedValue: setIsMenuOpen,
-  } = useBooleanState(false);
 
   const authObserver = userStore((state) => state.authObserver);
   const resetUser = userStore((state) => state.resetUser);
@@ -30,7 +23,6 @@ const Header = () => {
     await signOutForSB();
     resetUser();
     navigate('/main');
-    setIsMenuOpen(false);
     toggleMenu();
   };
 
@@ -71,29 +63,12 @@ const Header = () => {
         </h1>
       </div>
       {user !== null ? (
-        <div className="relative">
-          <button
-            onBlur={toggleIsMenuOpen}
-            onClick={toggleIsMenuOpen}
-            className="flex-center w-[70px] h-[50px]"
-          >
-            <img
-              src={user.profileImg !== null ? user.profileImg : ic_profile_3x}
-              alt="프로필 이미지"
-              className=" w-[37px] h-[37px] object-cover rounded-full border"
-            />
-          </button>
-          {isMenuOpen && (
-            <div
-              onMouseDown={(e) => {
-                e.preventDefault();
-              }}
-              onClick={onClickSignOutHandler}
-              className=" absolute -bottom-[55px] right-[10px] w-[100px] rounded-md cursor-pointer"
-            >
-              <img src={signOutDropDown} alt="로그아웃" />
-            </div>
-          )}
+        <div className="flex-center w-[70px] h-[50px]">
+          <img
+            src={user.profileImg !== null ? user.profileImg : ic_profile_3x}
+            alt="프로필 이미지"
+            className=" w-[37px] h-[37px] object-cover rounded-full border"
+          />
         </div>
       ) : pathname === '/signin' ? (
         <button

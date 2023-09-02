fee94ed3cdd7bbcb3ab57dfc9b52d7f6c1334493
Park kitae <bkt4044@naver.com>
2023-09-02 16:54:04 +0900

    Chore: 머지

--- src/components/common/sideBar/SideBar.tsx
+++ src/components/common/sideBar/SideBar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/return-await */
 /* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
---
+++ src/utils/calcDutchPay.ts
@@ -0,0 +1,33 @@
+export const calcDutchPay = async () => {
+  //   try {
+  // const realResult = await calcCostAndInsertPlansEnding(planId);
+  // console.log('가격 배열:', realResult);
+  // const budget = await getEndingCost(); // 예산을 가져오는 비동기 함수
+  // const invitees = await getMates(planId);
+  // const inviteesNumber = invitees.length;
+  //     if (
+  //       realResult !== null &&
+  //       realResult !== undefined &&
+  //       budget !== null &&
+  //       budget !== undefined &&
+  //       typeof budget.totalCost === 'number' &&
+  //       invitees !== null &&
+  //       invitees !== undefined
+  //     ) {
+  //       const totalCost = realResult.reduce((acc, val) => acc + val, 0);
+  //       const perPersonCost = totalCost / inviteesNumber;
+  //       const remainingBudget = budget.totalCost - perPersonCost;
+  //       console.log('예산:', budget.totalCost);
+  //       console.log('초대 인원:', invitees);
+  //       console.log('총 비용:', totalCost);
+  //       console.log('1인당 비용:', perPersonCost);
+  //       console.log('남은 예산:', remainingBudget);
+  //     }
+  //   } catch (error) {
+  //     console.error('에러 발생:', error);
+  //   }
+  //  };
+};
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+// calcDutchPay();

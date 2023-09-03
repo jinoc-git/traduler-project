74b7b0538de46ee9d780fa93bd379eeccd2220d2
sumin <ysm4260@gmail.com>
2023-09-03 21:58:20 +0900

    Design: subnav 아이콘 추가

--- src/components/common/nav/Nav.tsx
+++ src/components/common/nav/Nav.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 
+import IconEditDefault from '@assets/icons/IconEditDefault';
 import { modifyStateStore } from '@store/modifyStateStore';
 
 interface PropsType {
@@ -23,16 +24,19 @@ const Nav = ({ onClick, buttonDisabled, page }: PropsType) => {
   }, []);
 
   return (
-    <nav className="flex justify-between border-b-[1px] border-navy py-[11.5px]">
-      <div className="ml-[20px] text-blue_dark">여행 계획 시작</div>
-      <button
-        className="mr-[80px] text-blue_dark"
-        onClick={onClick}
-        type="submit"
-        disabled={buttonDisabled}
-      >
-        {modifyState === 'modify' ? `저장하기` : `수정하기`}
-      </button>
+    <nav className="flex justify-between border-b-[1px] border-navy py-[11.5px] items-center">
+      <div className="ml-[20px] text-navy_dark">여행 계획 시작</div>
+      <div className="flex items-center gap-2">
+        <IconEditDefault w="16" h="16" fill="#162F70" />
+        <button
+          className="mr-[80px] text-navy_dark"
+          onClick={onClick}
+          type="submit"
+          disabled={buttonDisabled}
+        >
+          {modifyState === 'modify' ? `저장하기` : `수정하기`}
+        </button>
+      </div>
     </nav>
   );
 };

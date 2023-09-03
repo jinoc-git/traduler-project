cc38694301cb629fb3aed6e06a3d350e45b93516
Park kitae <bkt4044@naver.com>
2023-09-03 15:31:39 +0900

    Fix: getQueryData를 사용해서 utils 함수를 불러 왔을 시 Data가 계속 들어오지 않는 오류 수정

--- src/utils/calcDutchPay.ts
+++ src/utils/calcDutchPay.ts
@@ -1,41 +1,28 @@
 import { calcCostAndInsertPlansEnding } from '@api/endingData';
 import { getTotalCost } from '@api/plans';
-import { useQueryClient } from '@tanstack/react-query';
-import { type UserType } from 'types/supabase';
 
-export const calcDutchPay = async (planId: string, userId: string) => {
-  const queryClient = useQueryClient();
-
+export const calcDutchPay = async (planId: string, countPeople: number) => {
   try {
     const dailyPaySum = await calcCostAndInsertPlansEnding(planId);
-    const endingTotalCost = await getTotalCost(userId);
-    const invitePeople = queryClient.getQueryData<UserType[]>([
-      'planMates',
-      planId,
-    ]);
+    const endingTotalCost = await getTotalCost(planId);
 
-    console.log('invitePeople: ', invitePeople);
     console.log('하루 지출 비용 합산 배열:', dailyPaySum);
 
-    if (
-      dailyPaySum === undefined ||
-      invitePeople === undefined ||
-      endingTotalCost === null
-    ) {
+    if (dailyPaySum === undefined || endingTotalCost === null) {
       return;
     }
 
-    const totalCost = dailyPaySum.reduce((acc, val) => acc + val, 0);
-    const perPersonCost = totalCost / invitePeople.length;
+    const totalPay = dailyPaySum.reduce((acc, val) => acc + val, 0);
+    const perPersonCost = totalPay / countPeople;
     const remainingBudget = endingTotalCost - perPersonCost;
 
     console.log('예산:', endingTotalCost);
-    console.log('초대 인원:', invitePeople.length);
-    console.log('총 비용:', totalCost);
+    console.log('초대 인원:', countPeople);
+    console.log('총 비용:', totalPay);
     console.log('1인당 비용:', perPersonCost);
     console.log('남은 예산:', remainingBudget);
 
-    return remainingBudget;
+    return { remainingBudget, dailyPaySum, totalPay, perPersonCost };
   } catch (error) {
     console.error(error);
   }

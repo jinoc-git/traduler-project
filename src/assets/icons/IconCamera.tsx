cd229fd0d3014adcb11a3c440a8d518a16fadeeb
Nick YOOO <outgrowthr@gmail.com>
2023-09-04 00:37:12 +0900

    Chore: 아이콘 생성

--- src/assets/icons/IconCamera.tsx
+++ src/assets/icons/IconCamera.tsx
@@ -22,11 +22,3 @@ const IconCamera = ({ w, h, fill }: IconType) => {
 };
 
 export default IconCamera;
-
-<svg
-  width="21"
-  height="17"
-  viewBox="0 0 21 17"
-  fill="none"
-  xmlns="http://www.w3.org/2000/svg"
-></svg>;

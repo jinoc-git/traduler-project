f9a9737f06ad3b40eb0a770e57b332b3ca610089
Nick YOOO <outgrowthr@gmail.com>
2023-09-05 17:36:36 +0900

    Design: 404Page UI 생성 Feat: 경로이동 / 사이드바 invisible

--- src/components/404/NotFoundFull.tsx
+++ src/components/404/NotFoundFull.tsx
@@ -1,37 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { NotFoundImg } from '@assets/index';
+import { sideBarStore } from '@store/sideBarStore';
 
 const NotFoundFull = () => {
-  const goToMain = () => (window.location.href = '/main');
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/welcome');
+  };
+
+  const goToBack = () => {
+    navigate(-1);
+  };
+
+  const setVisibilityIcon = sideBarStore((state) => state.setVisibilityIcon);
 
   const backgroundStyle: React.CSSProperties = {
     backgroundImage: `url('${NotFoundImg}')`,
-    backgroundSize: '50%', // 이미지를 커버하도록 설정
-    backgroundPosition: 'center', // 이미지를 가운데 정렬
-    backgroundRepeat: 'no-repeat', // 배경 이미지 반복 제거
-    minHeight: '100vh', // 최소 높이를 화면 높이와 같도록 설정
+    backgroundSize: '80%',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100vh',
   };
 
-  // useEffect(() => {
-  //   return () => {};
-  // }, []);
+  useEffect(() => {
+    setVisibilityIcon(false);
+  }, []);
 
   return (
     <div
-      className="min-h-screen flex items-center justify-center"
+      className="min-h-screen flex flex-col items-center justify-center"
       style={backgroundStyle}
     >
-      <h1 className="text-center text-black">저런.. 길을 잃으셨군요!</h1>
-      <p>존재하지 않는 주소를 입력하셨거나,</p>
-      <p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</p>
-      <button
-        onClick={goToMain}
-        className="cursor-pointer bg-white text-5E9FFF"
-      >
-        이전 페이지
-      </button>
-      <button className="bg-[5E9FFF] text-white px-4 py-2 mt-3">홈으로</button>
+      <div className="mt-[240px]">
+        <h1 className="text-center mb-4 text-gray_dark_2 text-xlg font-semibold">
+          저런.. 길을 잃으셨군요!
+        </h1>
+        <p className="text-center mb-4 text-gray_dark_1">
+          존재하지 않는 주소를 입력하셨거나,
+        </p>
+        <p className="text-center mb-4 text-gray_dark_1">
+          요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.
+        </p>
+
+        <div className="flex justify-center gap-4">
+          <button
+            onClick={goToMain}
+            className="cursor-pointer w-[140px] h-[36px] rounded-lg bg-white text-blue border border-blue hover:bg-blue_light_1 hover:text-blue_dark hover:border-blue_dark"
+          >
+            이전 페이지
+          </button>
+          <button
+            onClick={goToBack}
+            className="cursor-pointer w-[140px] h-[36px] rounded-lg bg-gradient-to-r from-blue_dark to-blue text-white border border-blue_dark hover:bg-white hover:text-blue_dark hover:border-blue_dark"
+          >
+            홈으로
+          </button>
+        </div>
+      </div>
     </div>
   );
 };

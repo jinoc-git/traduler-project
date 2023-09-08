3d57c207d086947a1e9726c54d34b30bcb55a3c3
sumin <ysm4260@gmail.com>
2023-09-08 17:27:41 +0900

    Fix: 캘린더 시작 날짜 제한 설정

--- src/components/addPlan/calendar/Calendar.tsx
+++ src/components/addPlan/calendar/Calendar.tsx
@@ -22,6 +22,7 @@ const Calendar: React.FC<CalendarProps> = ({
   EndDateChangeHandler,
 }) => {
   const { modifyState, clearRequiredDates } = modifyStateStore();
+  const today = new Date();
 
   useEffect(() => {
     return () => {
@@ -43,7 +44,7 @@ const Calendar: React.FC<CalendarProps> = ({
         onChange={(date) => {
           StartDateChangeHandler(date);
         }}
-        // minDate={today}
+        minDate={today}
         maxDate={endDate}
         selectsStart
         startDate={startDate}

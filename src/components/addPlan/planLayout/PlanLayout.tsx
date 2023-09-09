c993b996cbe7274544339fff4ed02398e09df6de
Park kitae <bkt4044@naver.com>
2023-09-10 01:44:08 +0900

    Fix: 반응형 화면 전환 후 addplan 가운데로 정렬하도록 수정 완료

--- src/components/addPlan/planLayout/PlanLayout.tsx
+++ src/components/addPlan/planLayout/PlanLayout.tsx
@@ -3,9 +3,9 @@ import React, { type ReactNode } from 'react';
 const PlanLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div
-      className="flex flex-col justify-center  
+      className="flex flex-col mx-auto
       sm:mt-[32px] sm:w-[320px]
-      md:mt-[100px] md:w-plan md:mx-auto"
+      md:mt-[100px] md:w-plan"
     >
       {children}
     </div>

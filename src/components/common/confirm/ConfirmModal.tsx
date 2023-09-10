6ce8328a2e1cdb12cff5f836c5884cc5b947f42d
Nick YOOO <outgrowthr@gmail.com>
2023-09-10 18:35:49 +0900

    Design: confirm Modal 반응형 추가

--- src/components/common/confirm/ConfirmModal.tsx
+++ src/components/common/confirm/ConfirmModal.tsx
@@ -31,17 +31,30 @@ const ConfirmModal = () => {
   return (
     <ModalLayout>
       <div className="text-lg font-bold text-navy_dark">{title}</div>
-      <div className="mb-6 text-gray_dark_1">{desc}</div>
-      <div className="flex">
+      <div
+        className=" text-gray_dark_1
+      md:mb-6
+      sm:mb-3
+      "
+      >
+        {desc}
+      </div>
+      <div className="flex justify-center">
         <button
           onClick={closeConfirm}
-          className="border border-navy text-navy rounded-lg px-[20px] py-[14px] w-[210px] mr-[24px] hover:bg-navy_light_1 duration-200"
+          className="border border-navy text-navy rounded-lg  hover:bg-navy_light_1 duration-200
+          md:px-[20px] md:py-[14px] md:w-[210px] md:mr-[24px]
+          sm:px-[10px] sm:py-[7px] sm:w-[105px] sm:mr-[12px]
+          "
         >
           취소
         </button>
         <button
           onClick={handleConfirm}
-          className="bg-navy text-white rounded-lg hover:bg-navy_light_3  px-[20px] py-[14px] disabled:bg-gray w-[210px] duration-200"
+          className="bg-navy text-white rounded-lg hover:bg-navy_light_3 disabled:bg-grayduration-200
+          md:px-[20px] md:py-[14px] md:w-[210px]
+          sm:px-[10px] sm:py-[7px] sm:w-[105px] 
+          "
         >
           {button()}
         </button>
--- src/components/plan/addPlan/ModalLayout.tsx
+++ src/components/plan/addPlan/ModalLayout.tsx
@@ -3,7 +3,12 @@ import React, { type ReactNode } from 'react';
 const MapModalLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center w-screen h-screen bg-black/30 animate-fadeIn">
-      <div className="flex flex-col justify-center bg-white w-modal h-modal rounded-lg px-[40px] py-[36px] gap-[16px]">
+      <div
+        className="flex flex-col justify-center bg-white rounded-lg
+      md:w-modal md:h-[225px]  md:px-[40px] md:py-[36px] md:gap-[16px]
+      sm:w-[300px] sm:h-[160px] sm:px-[20px] sm:py-[18px] sm:gap-[8px] 
+      "
+      >
         {children}
       </div>
     </div>

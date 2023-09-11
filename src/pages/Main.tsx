a0798437f8dd653c8385acbf5ff8c580ddfe4596
Park kitae <bkt4044@naver.com>
2023-09-11 20:10:14 +0900

    Design: Main layout 수정

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -28,10 +28,14 @@ const Main = () => {
       md:h-[363px]"
       ></div>
       <Profile />
-      <section className="flex main-layout ">
+      <section
+        className="flex mx-auto my-0
+      sm:w-[320px] 
+      md:w-[800px] "
+      >
         <button
           className="group flex items-center font-Bold justify-center rounded-[7px] hover:text-blue_dark gap-3 hover:bg-white
-          sm:w-[310px] sm:h-[46px] sm:mt-[16px] sm:mb-[26px] sm:ml-auto sm:font-bold sm:text-sm sm:text-blue_dark  sm:bg-white 
+          sm:w-[320px] sm:h-[46px] sm:mt-[16px] sm:mb-[26px] sm:ml-auto sm:font-bold sm:text-sm sm:text-blue_dark  sm:bg-white 
           md:md:w-[160px] md:h-[45px] mt-[35px] md:ml-auto md:border md:border-white md:text-white md:bg-blue_dark md:fill-white"
           onClick={() => {
             navigate('/addPlan');

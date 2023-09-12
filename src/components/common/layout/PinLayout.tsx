2e8b47862feadeb3b1eddc2302c08b61e6306e29
jinoc <choiae369@gmail.com>
2023-09-12 11:40:03 +0900

    Design: 레이아웃 수정

--- src/components/common/layout/PinLayout.tsx
+++ src/components/common/layout/PinLayout.tsx
@@ -42,14 +42,14 @@ const PinLayout = ({
   return (
     <div
       className="relative flex items-center justify-between gap-4
-    sm:w-[286px] sm:mb-[37px] sm:mx-auto
+    sm:w-[286px] sm:mb-[37px] 
     md:w-[651px] md:mx-[25px]"
     >
       <div className="flex items-center">
         <div
           className="absolute -z-10 border border-l-black 
-        sm:h-[82px] sm:translate-x-[15px] 
-        md:h-[147px] md:translate-x-[17.5px]"
+        sm:h-[102px] sm:translate-x-[15px] 
+        md:h-[147px] md:translate-x-[17.5px] md:translate-y-[0px]"
         />
         <p
           className="rounded-full bg-gradient-to-r from-[#5E9fff] from-0% to-[#1a68db] via-100%  text-center font-semibold text-white border-[5px] border-white

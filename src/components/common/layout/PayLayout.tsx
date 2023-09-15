fa5b79a9d9a7ab4f0551d38b28fe98eee6a2434f
jinoc <choiae369@gmail.com>
2023-09-15 20:22:15 +0900

    Design: 사이드바 디자인 수정
    - 모바일 사이드바 로그아웃 버튼 아래 마진 추가
    - 사이드바 기본으로 닫혀있도록 설정
    - SearchPeople.tsx 리팩토링
    - 동행 초대 모달 검색 결과나 초대 목록 수에 따라 스크롤바 생성으로 변경

--- src/components/common/layout/PayLayout.tsx
+++ src/components/common/layout/PayLayout.tsx
@@ -4,8 +4,8 @@ const PayLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div
       className="flex items-center font-semibold text-gray_dark_1 
-    sm:w-[286px] sm:h-[27px] sm:text-sm sm:mx-auto sm:mt-[16px]
-    md:w-[700px] md:text-[16px]  md:mb-[15px]  md:mx-[6px] "
+        sm:w-[286px] sm:h-[27px] sm:text-sm sm:mx-auto sm:mt-[11px]
+        md:w-[700px] md:text-[16px]  md:mb-[15px]  md:mx-[6px] "
     >
       {children}
     </div>

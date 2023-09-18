8b5889fe7247860779912a10cfe83af60e45b4f4
Park kitae <bkt4044@naver.com>
2023-09-18 16:15:12 +0900

    Fix: 스크롤락 터치스크린 수정

--- src/utils/withScrollLock.ts
+++ src/utils/withScrollLock.ts
@@ -1,35 +1,12 @@
 // 스크롤 잠금
+const eventFunc = (event: TouchEvent) => {
+  event.preventDefault();
+};
 export const enableScrollLock = () => {
-  const { body } = document;
-
-  if (body.getAttribute('scrollY') == null) {
-    const pageY = window.pageYOffset;
-
-    body.setAttribute('scrollY', pageY.toString());
-
-    body.style.overflow = 'hidden';
-    body.style.position = 'fixed';
-    body.style.left = '0px';
-    body.style.right = '0px';
-    body.style.bottom = '0px';
-    body.style.top = `-${pageY}px`;
-  }
+  document.addEventListener('touchmove', eventFunc, { passive: false });
 };
 
 // 스크롤 잠금 해제
 export const disableScrollLock = () => {
-  const { body } = document;
-
-  if (body.getAttribute('scrollY') != null) {
-    body.style.removeProperty('overflow');
-    body.style.removeProperty('position');
-    body.style.removeProperty('top');
-    body.style.removeProperty('left');
-    body.style.removeProperty('right');
-    body.style.removeProperty('bottom');
-
-    window.scrollTo(0, Number(body.getAttribute('scrollY')));
-
-    body.removeAttribute('scrollY');
-  }
+  document.removeEventListener('touchmove', eventFunc);
 };
